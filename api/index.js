aa87bf7cbc217e3e9788da24674d8b78b22c2892
Swapnil <swapnillandage79@gmail.com>
2024-02-16 06:54:08 +0530

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,17 @@
-import express from "express";
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 const app = express();
-
+const port = 8000;
+mongoose
+  .connect(process.env.MONGO)
+  .then((x) => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error while connecting to db", error);
+  });
 app.listen(3000, () => {
-  console.log("server is running on 301100");
+  console.log(`server is running on  ${port}`);
 });

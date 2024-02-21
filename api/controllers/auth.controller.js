7c291d4c5cd4bf9d1ccdc3d20a50f5636090418a
Swapnil <swapnillandage79@gmail.com>
2024-02-21 16:34:05 +0530

    created singin api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,8 +1,8 @@
 import { errorHandler } from "../utils/error.js";
 import User from "./../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 export const signup = async (req, res, next) => {
-  
   const { username, email, password } = req.body;
   if (
     !username ||
@@ -44,5 +44,34 @@ export const signup = async (req, res, next) => {
   } catch (error) {
     next(error);
     // res.status(500).json({ message: error.message });
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    next(errorHandler(400, "All field are required"));
+  }
+  try {
+    const validUser = await User.findOne({ email: email });
+    if (!validUser) {
+      return next(errorHandler(404, "User Not Found"));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(400, "Enter a Valid password"));
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET_KEY, {
+      expiresIn: "24h",
+    });
+    //setting token to browser cookies.
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json(validUser);
+  } catch (error) {
+    next(error);
   }
 };

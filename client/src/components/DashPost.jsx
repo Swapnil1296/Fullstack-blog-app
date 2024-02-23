7faba7a347e9b2ad6daee6032e9cfaf180165dc7
Swapnil <swapnillandage79@gmail.com>
2024-02-23 17:19:26 +0530

    added show more functionality to dashpost

--- client/src/components/DashPost.jsx
+++ client/src/components/DashPost.jsx
@@ -5,15 +5,40 @@ import { Link } from "react-router-dom";
 
 const DashPost = () => {
   const { currentUser } = useSelector((state) => state.user);
-  const [userPosts, setUserPosts] = useState([]);
-  console.log(userPosts);
-  useEffect(() => {
-    const fetchPosts = async () => {
+    const [userPosts, setUserPosts] = useState([]);
+    const [showMore, setShowMore] = useState(true);
+
+    useEffect(() => {
+      const fetchPosts = async () => {
+        try {
+          const res = await fetch(
+            `/api/post/getposts?userId=${currentUser._id}`
+          );
+          const data = await res.json();
+          if (res.ok) {
+            setUserPosts(data.posts);
+            if (data.posts.length < 9) {
+              setShowMore(false);
+            }
+          }
+        } catch (error) {
+          console.log(error.message);
+        }
+      };
+      if (currentUser.isAdmin) {
+        fetchPosts();
+      }
+    }, [currentUser._id]);
+    const handleShowMore = async () => {
+      const startIndex = userPosts.length;
       try {
-        const res = await fetch(`/api/post/getposts?userId=${currentUser._id}`);
+        const res = await fetch(
+          `/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`
+        );
         const data = await res.json();
+        console.log(data);
         if (res.ok) {
-          setUserPosts(data.posts);
+          setUserPosts((prev) => [...prev, ...data.posts]);
           if (data.posts.length < 9) {
             setShowMore(false);
           }
@@ -22,68 +47,79 @@ const DashPost = () => {
         console.log(error.message);
       }
     };
-    if (currentUser.isAdmin) {
-      fetchPosts();
-    }
-  }, [currentUser._id]);
-  return (
-    <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
-      {currentUser.isAdmin && userPosts.length > 0 ? (
-        <Table hoverable className="shadow-md">
-          <Table.Head>
-            <Table.HeadCell>Date Update</Table.HeadCell>
-            <Table.HeadCell>Post's Image</Table.HeadCell>
-            <Table.HeadCell>Post's Title</Table.HeadCell>
-            <Table.HeadCell>Categaory</Table.HeadCell>
-            <Table.HeadCell>Delete</Table.HeadCell>
-            <Table.HeadCell>
-              <span>Edit</span>
-            </Table.HeadCell>
-          </Table.Head>
-          {userPosts.map((post) => (
-            <Table.Body className="divide-y">
-              <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-                <Table.Cell>
-                  {new Date(post.updatedAt).toLocaleDateString()}
-                </Table.Cell>
-                <Table.Cell>
-                  <Link to={`/post/${post.slug}`}>
-                    <img
-                      src={post.image}
-                      alt={post.title}
-                      className="w-20 h-10 object-cover bg-gray-500"
-                    />
-                  </Link>
-                </Table.Cell>
 
-                <Table.Cell>
-                  <Link
-                    to={`/post/${post.slug}`}
-                    className="font-medium text-gray-900 dark:text-white"
-                  >
-                    {post.title}
-                  </Link>
-                </Table.Cell>
-                <Table.Cell className="capitalize">{post.category}</Table.Cell>
-                <Table.Cell>
-                  <span className="font-medium text-red-500 hover:underline cursor-pointer">
-                    Delete
-                  </span>
-                </Table.Cell>
-                <Table.Cell>
-                  <Link to={`/update-post/${post._id}`}>
-                    <span className="text-teal-500 hover:underline">Edit</span>
-                  </Link>
-                </Table.Cell>
-              </Table.Row>
-            </Table.Body>
-          ))}
-        </Table>
-      ) : (
-        <p>You dont hanve any posts yet</p>
-      )}
-    </div>
-  );
+    return (
+      <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
+        {currentUser.isAdmin && userPosts.length > 0 ? (
+          <>
+            <Table hoverable className="shadow-md">
+              <Table.Head>
+                <Table.HeadCell>Date Update</Table.HeadCell>
+                <Table.HeadCell>Post's Image</Table.HeadCell>
+                <Table.HeadCell>Post's Title</Table.HeadCell>
+                <Table.HeadCell>Categaory</Table.HeadCell>
+                <Table.HeadCell>Delete</Table.HeadCell>
+                <Table.HeadCell>
+                  <span>Edit</span>
+                </Table.HeadCell>
+              </Table.Head>
+              {userPosts.map((post) => (
+                <Table.Body className="divide-y" key={post._id}>
+                  <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                    <Table.Cell>
+                      {new Date(post.updatedAt).toLocaleDateString()}
+                    </Table.Cell>
+                    <Table.Cell>
+                      <Link to={`/post/${post.slug}`}>
+                        <img
+                          src={post.image}
+                          alt={post.title}
+                          className="w-20 h-10 object-cover bg-gray-500"
+                        />
+                      </Link>
+                    </Table.Cell>
+
+                    <Table.Cell>
+                      <Link
+                        to={`/post/${post.slug}`}
+                        className="font-medium text-gray-900 dark:text-white"
+                      >
+                        {post.title}
+                      </Link>
+                    </Table.Cell>
+                    <Table.Cell className="capitalize">
+                      {post.category}
+                    </Table.Cell>
+                    <Table.Cell>
+                      <span className="font-medium text-red-500 hover:underline cursor-pointer">
+                        Delete
+                      </span>
+                    </Table.Cell>
+                    <Table.Cell>
+                      <Link to={`/update-post/${post._id}`}>
+                        <span className="text-teal-500 hover:underline">
+                          Edit
+                        </span>
+                      </Link>
+                    </Table.Cell>
+                  </Table.Row>
+                </Table.Body>
+              ))}
+            </Table>
+            {showMore && (
+              <button
+                onClick={handleShowMore}
+                className="w-full text-teal-500 self-center text-sm py-7"
+              >
+                Show more
+              </button>
+            )}
+          </>
+        ) : (
+          <p>You dont hanve any posts yet</p>
+        )}
+      </div>
+    );
 };
 
 export default DashPost;

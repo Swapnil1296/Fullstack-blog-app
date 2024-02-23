83d12e04c51b47a52b1caaf7e5b500936fcfa8b8
Swapnil <swapnillandage79@gmail.com>
2024-02-23 17:41:36 +0530

    added delete posts functionality

--- api/routes/createPostRoute.js
+++ api/routes/createPostRoute.js
@@ -1,10 +1,17 @@
 import express from "express";
-import { creatPost, getPosts } from "../controllers/post.controller.js";
+import {
+  creatPost,
+  deletepost,
+  getPosts,
+  updatepost,
+} from "../controllers/post.controller.js";
 import { verifyToken } from "./../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, creatPost);
 router.get("/getposts", getPosts);
+router.delete("/deletepost/:postId/:userId", verifyToken, deletepost);
+router.put("/updatepost/:postId/:userId", verifyToken, updatepost);
 
 export default router;
